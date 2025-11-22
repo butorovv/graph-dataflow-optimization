75a52ec3d806fcfb70ea9eb1ddcd0dd1f9df31bc
butorovv <george.butorov@mail.ru>
2025-11-22 23:52:06 +0300

    fixes dijkstra, add weights

--- include/infrastructure/BGLShortestPath.h
+++ include/infrastructure/BGLShortestPath.h
@@ -1,26 +1,38 @@
-// include/infrastructure/BGLShortestPath.h
 #ifndef BGLSHORTESTPATH_H
 #define BGLSHORTESTPATH_H
+
 #include "../domain/NetworkGraph.h"
 #include "../domain/NetworkTypes.h"
+#include "../domain/WeightCalculator.h"
 #include "IPathFinder.h"
+#include <chrono>
 
 namespace Infrastructure
 {
     class BGLShortestPath : public IPathFinder
     {
     public:
-        BGLShortestPath() = default;
+        BGLShortestPath(bool useWeights = true, 
+                       Domain::WeightCalculator::Strategy strategy = Domain::WeightCalculator::MINIMIZE_LATENCY)
+            : useWeights(useWeights), strategy(strategy) {}
+        
         ~BGLShortestPath() override = default;
 
         Domain::PathResult findShortestPath(
             const Domain::NetworkGraphPtr &graph,
             int start_id, int end_id) override;
+
+        // статический метод с параметрами
         static Domain::PathResult findShortestPathStatic(
             const Domain::NetworkGraphPtr &graph,
-            int start_id, int end_id);
-    };
+            int start_id, int end_id,
+            bool useWeights = true,
+            Domain::WeightCalculator::Strategy strategy = Domain::WeightCalculator::MINIMIZE_LATENCY);
 
+    private:
+        bool useWeights;
+        Domain::WeightCalculator::Strategy strategy;
+    };
 }
 
 #endif
